de248a06edc60d3c4c796428669cc964700dda7e
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-07-11 08:41:10 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 # Copyright (C) 2021 kenkannih/Pengganti-Bot
 #
-# Apache License Version 2.0, January 2004 http://www.apache.org/licenses/
+# Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
+       http://www.apache.org/licenses/LICENSE-2.0
 # Pengganti-Bot
 import os
 import logging
